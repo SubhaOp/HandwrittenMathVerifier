ed4f5395d1a741a81278eb931ffc057cdd1cd326
Subhajit Paik <subhajitpaik6@gmail.com>
2026-07-30 20:38:41 +0530

    Add dropout to reduce overfitting. Explicitly flatten LSTM parameters (improves CUDA performance).

--- src/model.py
+++ src/model.py
@@ -8,21 +8,37 @@ class MathRecognizer(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
 
+        # ==========================================
+        # CNN Backbone
+        # ==========================================
+
         backbone = models.resnet18(
             weights=models.ResNet18_Weights.DEFAULT
         )
 
+        # Remove Average Pool and FC layers
         self.cnn = nn.Sequential(
             *list(backbone.children())[:-2]
         )
+
+        # ==========================================
+        # BiLSTM
+        # ==========================================
 
         self.lstm = nn.LSTM(
             input_size=512 * 4,
             hidden_size=256,
             num_layers=2,
             batch_first=True,
-            bidirectional=True
+            bidirectional=True,
+            dropout=0.3
         )
+
+        # ==========================================
+        # Classifier
+        # ==========================================
+
+        self.dropout = nn.Dropout(0.3)
 
         self.fc = nn.Linear(
             512,
@@ -31,16 +47,30 @@ class MathRecognizer(nn.Module):
 
     def forward(self, x):
 
+        # CNN Feature Extraction
         x = self.cnn(x)
 
-        b, c, h, w = x.shape
+        # Shape:
+        # (B, 512, 4, W)
 
+        b, c, h, w = x.size()
+
+        # Convert width dimension into sequence
         x = x.permute(0, 3, 1, 2)
 
+        # (B, W, 512*4)
         x = x.reshape(b, w, c * h)
 
+        # Better CUDA performance
+        self.lstm.flatten_parameters()
+
+        # BiLSTM
         x, _ = self.lstm(x)
 
+        # Dropout
+        x = self.dropout(x)
+
+        # Character classification
         x = self.fc(x)
 
         return x
